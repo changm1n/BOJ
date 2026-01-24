72988271504767210ef0c43dfb665ec6b754cafc
창민 <134192668+changm1n@users.noreply.github.com>
2026-01-24 18:05:43 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 112 ms, Memory: 110920 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,10 +1,16 @@
 T = int(input())
 coin_types = [25,10,5,1]
 count = []
+a = []
 for i in range(T):
     n = int(input())
-    for coin in coin_types:
-        print((n // coin),end=" ")
-        n = n%coin
-    count = []
+    a.append(n)
 
+for i in range(len(a)):
+    for coin in coin_types:
+        count.append(a[i] // coin)
+        a[i] %= coin
+    for j in range(4):
+        print(count[j],end=" ")
+    print("")
+    count = []
